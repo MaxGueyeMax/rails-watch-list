2a527fe39a0a415d20e663e1592176773b2b66e9
Maxime Gueye <gueye.maxime1994@gmail.com>
2022-08-19 16:04:04 +0200

    Final exo

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,12 +17,12 @@ shawshank = Movie.create!(title: "The Shawshank Redemption", overview: "Framed i
 Movie.create!(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 Movie.create!(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 
-puts "Creating lists..."
-horror = List.create!(name: "Horror")
+# puts "Creating lists..."
+# horror = List.create!(name: "Horror")
 
-drama = List.create!(name: "Drama")
+# drama = List.create!(name: "Drama")
 
-comedy = List.create!(name: "Comedy")
+# comedy = List.create!(name: "Comedy")
 
 puts "Creating bookmarks..."
 bookmark_1 = Bookmark.create!(
